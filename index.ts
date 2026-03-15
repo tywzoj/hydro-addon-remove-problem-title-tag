1b209b2087950c79bdf798f43de15a687cd3496d
Antares <antares.mi@outlook.com>
2026-03-15 23:35:00 +0800

    fix: nav

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import type { Context, Handler, ProblemDict } from "hydrooj";
 import { _ } from "hydrooj";
+import type { ContestDetailHandler } from "hydrooj/src/handler/contest";
 import type { ProblemDetailHandler } from "hydrooj/src/handler/problem";
 
 export function apply(ctx: Context) {
@@ -19,6 +20,16 @@ export function apply(ctx: Context) {
     };
     ctx.on("handler/after/ContestProblemList#get", pdictHandler);
     ctx.on("handler/after/ContestScoreboard#get", pdictHandler);
+
+    ctx.on("handler/after/ContestDetail", (handler: ContestDetailHandler) => {
+        if (handler.response.body.overrideNav) {
+            (handler.response.body.overrideNav as { name: string; displayName?: string }[]).forEach((item) => {
+                if (item.name === "problem_detail" && item.displayName) {
+                    item.displayName = removeTitleTag(item.displayName);
+                }
+            });
+        }
+    });
 }
 
 function removeTitleTag(s: string): string {
